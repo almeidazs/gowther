299b06ac4f922d0b825afb8a06ce2f745838acac
Victor Albuquerque <albq.victor@gmail.com>
2025-12-13 19:10:32 -0300

    fix(cli): changes on cli desc

--- cmd/root.go
+++ cmd/root.go
@@ -3,9 +3,9 @@ package cmd
 import "github.com/spf13/cobra"
 
 var rootCmd = &cobra.Command{
-	Use:   "golint",
-	Short: "A modern linter for Go, inspired by Biome",
-	Long:  "Fast, opinionated linter and formatter for Go with auto-fix capabilities",
+	Use:   "gowther",
+	Short: "A modern linter for Go",
+	Long:  "Fast, opinionated linter and formatter for Go with auto-fix capabilities", // TODO: adicionar uma desc mais detalhada com a usage
 }
 
 func Exec() {
