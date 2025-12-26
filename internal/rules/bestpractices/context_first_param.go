ad2fbd633d981627e555961e5c992dae93bb64a3
Victor Albuquerque <albq.victor@gmail.com>
2025-12-26 18:52:26 -0300

    wip(rules): making changes on context_first to follow the max_params arch

--- internal/rules/bestpractices/context_first_param.go
+++ internal/rules/bestpractices/context_first_param.go
@@ -6,6 +6,19 @@ import (
 	"github.com/serenitysz/serenity/internal/rules"
 )
 
+type ContextFirstRule struct{}
+
+func (c *ContextFirstRule) Name() string {
+	return "context-first-param"
+}
+
+func (c *ContextFirstRule) Targets() []ast.Node {
+	return []ast.Node{(*ast.FuncDecl, *ast.SelectorExpr, *ast.StarExpr)(nil)}
+}
+
+func (c *ContextFirstRule) Run(runner *rules.Runner, node ast.Node) {
+}
+
 func isContextType(expr ast.Expr) bool {
 	switch t := expr.(type) {
 	case *ast.SelectorExpr:
