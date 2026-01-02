6bdf2dcc60e95b4fae94b116bcfb68fd37f13465
almeidazs <2hiagoalmeida@gmail.com>
2026-01-02 19:53:57 -0300

    feat(empty-block): adding new rule

---
+++ internal/rules/correctness/empty_block.go
@@ -0,0 +1,47 @@
+package correctness
+
+import (
+	"go/ast"
+
+	"github.com/serenitysz/serenity/internal/rules"
+)
+
+type EmptyBlockRule struct{}
+
+func (r *EmptyBlockRule) Name() string {
+	return "empty-block"
+}
+
+func (r *EmptyBlockRule) Targets() []ast.Node {
+	return []ast.Node{(*ast.BlockStmt)(nil)}
+}
+
+func (r *EmptyBlockRule) Run(runner *rules.Runner, node ast.Node) {
+	if runner.ShouldStop != nil && runner.ShouldStop() {
+		return
+	}
+
+	correctness := runner.Cfg.Linter.Rules.Correctness
+
+	if correctness == nil || (correctness.Use != nil && !*correctness.Use) || correctness.EmptyBlock == nil {
+		return
+	}
+
+	maxIssues := rules.GetMaxIssues(runner.Cfg)
+
+	if maxIssues > 0 && *runner.IssuesCount >= maxIssues {
+		return
+	}
+
+	spec := node.(*ast.BlockStmt)
+
+	if len(spec.List) == 0 {
+		*runner.IssuesCount++
+
+		*runner.Issues = append(*runner.Issues, rules.Issue{
+			ID:       rules.EmptyBlockID,
+			Pos:      runner.Fset.Position(spec.Pos()),
+			Severity: rules.ParseSeverity(correctness.EmptyBlock.Severity),
+		})
+	}
+}
